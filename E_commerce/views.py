6d435d0b1f21717ce9c6a5710628d346e6295efa
Biraj Poudel <birajpoudel23@gmail.com>
2022-03-24 16:56:47 +0545

    Banners and sliders

--- E_commerce/views.py
+++ E_commerce/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import redirect, render
 
+from app.models import slider, Banner
 
 def BASE(request):
     return render(request,'base.html')
 
 
 def HOME(request):
-    return render(request, 'index.html')
+    sliders = slider.objects.all()
+    banners = Banner.objects.all()
+    context = {
+        'sliders':sliders,
+        'banners':banners
+
+    }
+    return render(request, 'index.html',context)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+STATUS=(('active','active'),('','default'))
+LABELS=(('special','special'),('','default'))
+
 
 # Create your models here.
+class slider(models.Model):
+    DISCOUNT_DEAL = (
+        ('HOT DEALS','HOT DEALS'),
+        ('New Arraivels','New Arraivels'),
+        ('New DEALS','New DEALS'))
+    Image = models.ImageField(upload_to='media/slider_imgs')
+    Discount_Deal = models.CharField(choices = DISCOUNT_DEAL,max_length=100)
+    SALE = models.IntegerField()
+    Brand_Name = models.CharField(max_length=500)
+    Discount = models.IntegerField()
+    Link = models.CharField(max_length=500)
+    Rank = models.IntegerField()
+    Status = models.CharField(max_length=300, choices=STATUS, blank=True)
+
+    def __str__(self):
+        return self.Brand_Name
+
+class Banner(models.Model):
+    Image = models.ImageField(upload_to='media/banner_imgs')
+    Discount_Deal = models.CharField(max_length=100)
+    Quote= models.CharField(max_length=400)
+    Discount = models.IntegerField()
+
+    def __str__(self):
+        return self.Quote
+
+
+
